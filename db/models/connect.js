ac03653692252036b3e51b451c477f861704f061
Mamta Gupta <mamtag644@gmail.com>
2021-05-19 14:21:32 +0530

    nodejsproject

---
+++ db/models/connect.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+ const dbOptions = {
+    useNewUrlParser : true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    poolSize:process.env.POOL_SIZE
+};
+mongoose.connect(process.env.DBURL,dbOptions,(err)=>{
+    if(err){
+        console.log("Connection Not Stablished");
+    }
+    else{
+        console.log("Connection Stablished");
+    }
+})
+
+module.exports=mongoose;
